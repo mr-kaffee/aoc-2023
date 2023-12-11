b7fe6586420a9cc6acd621e94f4698b28116da0f
Peter Wieland <peter@die-wielands.net>
2023-12-11 10:21:48 +0000

    Optimize day11/rust/peter
    Signed-off-by: Peter Wieland <peter@die-wielands.net>

---
+++ day11/rust/peter/benches/day11.rs
@@ -0,0 +1,11 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use mr_kaffee_2023_11::*;
+
+pub fn criterion_benchmark(c: &mut Criterion) {
+    let data = read_input().as_str().into();
+    c.bench_function("star_1", |b| b.iter(|| star_1(black_box(&data))));
+    c.bench_function("star_2", |b| b.iter(|| star_2(black_box(&data))));
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
--- day11/rust/peter/src/lib.rs
+++ day11/rust/peter/src/lib.rs
@@ -1,11 +1,10 @@
 use input::*;
-use mr_kaffee_utils::grids::Grid;
 use std::fs::read_to_string;
 
 // tag::prelude[]
 pub const IDENTIFIER: &str = "2023/11";
 
-pub type SolT = usize;
+pub type SolT = isize;
 pub type InputT = PuzzleData;
 
 pub fn read_input() -> String {
@@ -15,59 +14,79 @@ pub fn read_input() -> String {
 
 // tag::input[]
 pub mod input {
-    use mr_kaffee_utils::grids::{Grid, MakeGrid};
-
-    #[derive(Debug)]
-    pub struct PuzzleData(pub Grid);
+    #[derive(Debug, PartialEq, Eq, Default)]
+    pub struct PuzzleData {
+        pub galaxies: Vec<(usize, usize)>,
+        pub count_in_cols: Vec<usize>,
+        pub count_in_rows: Vec<usize>,
+    }
 
     impl<T> From<T> for PuzzleData
     where
-        T: AsRef<[u8]>,
+        T: AsRef<str>,
     {
         fn from(s: T) -> Self {
-            Self(s.make_grid(None))
+            s.as_ref()
+                .lines()
+                .enumerate()
+                .fold(PuzzleData::default(), |mut data, (row, line)| {
+                    data.count_in_rows.push(0);
+                    line.bytes().enumerate().filter(|(_, b)| b == &b'#').fold(
+                        data,
+                        |mut data, (col, _)| {
+                            data.galaxies.push((col, row));
+                            data.count_in_cols
+                                .resize(data.count_in_cols.len().max(col + 1), 0);
+                            data.count_in_cols[col] += 1;
+                            data.count_in_rows[row] += 1;
+                            data
+                        },
+                    )
+                })
         }
     }
 }
 // end::input[]
 
 // tag::star_1[]
-pub fn sum_shortest_path(grid: &Grid, expansion: SolT) -> SolT {
-    let empty_cols = (0..grid.width())
-        .filter(|&col| (0..grid.height()).all(|row| grid[(col, row)] == b'.'))
-        .collect::<Vec<_>>();
-    let empty_rows = (0..grid.height())
-        .filter(|&row| (0..grid.width()).all(|col| grid[(col, row)] == b'.'))
-        .collect::<Vec<_>>();
-    let galaxies = (0..grid.len())
-        .filter(|&idx| grid[idx] == b'#')
-        .map(|idx| grid.to_col_row(idx))
-        .map(|(col, row)| {
-            (
-                col + empty_cols.iter().take_while(|&&e_col| e_col < col).count() * (expansion - 1),
-                row + empty_rows.iter().take_while(|&&e_row| e_row < row).count() * (expansion - 1),
-            )
-        })
-        .collect::<Vec<_>>();
-
-    galaxies
+fn calc_offsets(counts: &[usize], expansion: SolT) -> Vec<SolT> {
+    counts
         .iter()
+        .scan(0, |cum_sum, &cnt| {
+            let val = Some(*cum_sum);
+            *cum_sum += if cnt > 0 { 1 } else { expansion };
+            val
+        })
+        .collect()
+}
+
+pub fn sum_shortest_path(data: &PuzzleData, expansion: SolT) -> SolT {
+    let col_offsets = calc_offsets(&data.count_in_cols, expansion);
+    let row_offsets = calc_offsets(&data.count_in_rows, expansion);
+    data.galaxies
+        .iter()
+        .map(|&(col_a, row_a)| (col_offsets[col_a], row_offsets[row_a]))
         .enumerate()
-        .flat_map(|(pos, &a)| galaxies[pos + 1..].iter().map(move |&b| (a, b)))
-        .map(|((col_a, row_a), (col_b, row_b))| {
-            col_b.max(col_a) - col_b.min(col_a) + row_b.max(row_a) - row_b.min(row_a)
+        .map(|(pos, (col_a, row_a))| {
+            data.galaxies[pos + 1..]
+                .iter()
+                .map(|&(col_a, row_a)| (col_offsets[col_a], row_offsets[row_a]))
+                .map(|(col_b, row_b)| {
+                    col_b.max(col_a) - col_b.min(col_a) + row_b.max(row_a) - row_b.min(row_a)
+                })
+                .sum::<SolT>()
         })
         .sum()
 }
 
-pub fn star_1(PuzzleData(grid): &PuzzleData) -> SolT {
-    sum_shortest_path(grid, 2)
+pub fn star_1(data: &PuzzleData) -> SolT {
+    sum_shortest_path(data, 2)
 }
 // end::star_1[]
 
 // tag::star_2[]
-pub fn star_2(PuzzleData(grid): &PuzzleData) -> SolT {
-    sum_shortest_path(grid, 1_000_000)
+pub fn star_2(data: &PuzzleData) -> SolT {
+    sum_shortest_path(data, 1_000_000)
 }
 // end::star_2[]
 
@@ -92,8 +111,24 @@ mod tests {
     pub fn test_from() {
         let data = PuzzleData::from(CONTENT);
         println!("{data:?}");
-        assert_eq!(10, data.0.width());
-        assert_eq!(10, data.0.height());
+        assert_eq!(
+            PuzzleData {
+                galaxies: vec![
+                    (3, 0),
+                    (7, 1),
+                    (0, 2),
+                    (6, 4),
+                    (1, 5),
+                    (9, 6),
+                    (7, 8),
+                    (0, 9),
+                    (4, 9)
+                ],
+                count_in_cols: vec![2, 1, 0, 1, 1, 0, 1, 2, 0, 1],
+                count_in_rows: vec![1, 1, 1, 0, 1, 1, 1, 0, 1, 2],
+            },
+            data
+        );
     }
 
     #[test]
@@ -103,9 +138,8 @@ mod tests {
 
     #[test]
     pub fn test_star_2() {
-        let PuzzleData(grid) = CONTENT.into();
-        assert_eq!(1_030, sum_shortest_path(&grid, 10));
-        assert_eq!(8_410, sum_shortest_path(&grid, 100));
+        assert_eq!(1_030, sum_shortest_path(&CONTENT.into(), 10));
+        assert_eq!(8_410, sum_shortest_path(&CONTENT.into(), 100));
     }
 }
 // end::tests[]
