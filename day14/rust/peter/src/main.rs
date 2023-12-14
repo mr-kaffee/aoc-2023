d0a15d3d768a4be38170e2241fc952722bac4f43
Peter Wieland <peter@die-wielands.net>
2023-12-14 07:25:36 +0000

    day14/rust/peter
    Signed-off-by: Peter Wieland <peter@die-wielands.net>

---
+++ day14/rust/peter/src/main.rs
@@ -0,0 +1,29 @@
+use mr_kaffee_2023_14::*;
+use std::time::Instant;
+
+fn main() {
+    let t = Instant::now();
+
+    let input = read_input();
+    let data = InputT::from(&input);
+
+    let t_1 = Instant::now();
+    let sol_1 = star_1(&data);
+    println!(
+        "Solved {}, star 1 in {:?}: {}",
+        IDENTIFIER,
+        t_1.elapsed(),
+        sol_1
+    );
+
+    let t_2 = Instant::now();
+    let sol_2 = star_2(&data);
+    println!(
+        "Solved {}, star 2 in {:?}: {}",
+        IDENTIFIER,
+        t_2.elapsed(),
+        sol_2
+    );
+
+    println!("Solved {} in {:?}", IDENTIFIER, t.elapsed());
+}
