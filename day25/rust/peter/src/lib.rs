ce372220985d822597305ccfc969a44136a54683
Peter Wieland <peter@die-wielands.net>
2024-01-01 11:56:32 +0000

    Optimize 2023/25
    Signed-off-by: Peter Wieland <peter@die-wielands.net>

--- day25/rust/peter/src/lib.rs
+++ day25/rust/peter/src/lib.rs
@@ -1,5 +1,4 @@
 use input::PuzzleData;
-use std::collections::{HashMap, HashSet, VecDeque};
 use std::fs::read_to_string;
 
 // tag::prelude[]
@@ -18,8 +17,8 @@ mod input {
 
     pub struct PuzzleData(pub Vec<Vec<usize>>);
 
-    impl<T: AsRef<str> + ?Sized> From<&T> for PuzzleData {
-        fn from(value: &T) -> Self {
+    impl<T: AsRef<str>> From<T> for PuzzleData {
+        fn from(value: T) -> Self {
             let mut adjacents = Vec::<Vec<usize>>::new();
             let mut indices = HashMap::new();
             for line in value.as_ref().lines() {
@@ -46,107 +45,75 @@ mod input {
 // end::input[]
 
 // tag::star_1[]
-pub fn get_shortest_path(
-    adjacents: &[Vec<usize>],
-    forbidden: &[Vec<(usize, usize)>],
-    start: usize,
-    target: usize,
-) -> Option<HashMap<usize, usize>> {
-    let mut seen = HashSet::from([start]);
-    let mut queue = VecDeque::from([start]);
-    let mut parents = HashMap::new();
-    while let Some(idx) = queue.pop_front() {
-        if idx == target {
-            return Some(parents);
-        }
+mod direct_solution {
+    use std::collections::{HashMap, HashSet, VecDeque};
 
-        for &adj in adjacents[idx].iter().filter(|&&adj| {
-            !forbidden
-                .iter()
-                .any(|forbidden| forbidden.contains(&(adj.min(idx), adj.max(idx))))
-        }) {
-            if seen.insert(adj) {
-                queue.push_back(adj);
-                parents.insert(adj, idx);
+    pub fn get_shortest_path(
+        adjacents: &[Vec<usize>],
+        forbidden: &[Vec<(usize, usize)>],
+        start: usize,
+        target: usize,
+    ) -> (Option<HashMap<usize, usize>>, usize) {
+        let mut seen = HashSet::from([start]);
+        let mut queue = VecDeque::from([start]);
+        let mut parents = HashMap::new();
+        let mut reached = 0;
+        while let Some(idx) = queue.pop_front() {
+            reached += 1;
+            if idx == target {
+                return (Some(parents), reached);
             }
-        }
-    }
 
-    None
-}
-
-pub fn get_target_with_three_paths(
-    adjacents: &[Vec<usize>],
-    start: usize,
-) -> (usize, [Vec<(usize, usize)>; 3]) {
-    let mut paths = [Vec::new(), Vec::new(), Vec::new()];
-    for target in 1..adjacents.len() - 1 {
-        // find three disjoint paths
-        for k in 0..3 {
-            let Some(parents) = get_shortest_path(adjacents, &paths[0..k], start, target) else {
-                panic!("Less than three paths from {} to {}!", start, target);
-            };
-            paths[k].clear();
-            let mut cur = target;
-            while let Some(&parent) = parents.get(&cur) {
-                let link = (cur.min(parent), cur.max(parent));
-                paths[k].push(link);
-                cur = parent;
+            for &adj in adjacents[idx].iter().filter(|&&adj| {
+                !forbidden
+                    .iter()
+                    .any(|forbidden| forbidden.contains(&(adj.min(idx), adj.max(idx))))
+            }) {
+                if seen.insert(adj) {
+                    queue.push_back(adj);
+                    parents.insert(adj, idx);
+                }
             }
         }
 
-        // check that there is no fourth path
-        if get_shortest_path(adjacents, &paths, start, target).is_none() {
-            return (target, paths);
-        }
+        (None, reached)
     }
 
-    panic!("No solution");
-}
+    pub fn get_partitions_connected_by_three_paths(
+        adjacents: &[Vec<usize>],
+        start: usize,
+    ) -> (usize, usize) {
+        let mut paths = [Vec::new(), Vec::new(), Vec::new()];
+        for target in 1..adjacents.len() - 1 {
+            // find three disjoint paths
+            for k in 0..3 {
+                let (Some(parents), _) = get_shortest_path(adjacents, &paths[0..k], start, target)
+                else {
+                    panic!("Less than three paths from {} to {}!", start, target);
+                };
+                paths[k].clear();
+                let mut cur = target;
+                while let Some(&parent) = parents.get(&cur) {
+                    let link = (cur.min(parent), cur.max(parent));
+                    paths[k].push(link);
+                    cur = parent;
+                }
+            }
 
-pub fn count_reachable_without_target(
-    adjacents: &[Vec<usize>],
-    forbidden: &[(usize, usize)],
-    start: usize,
-    target: usize,
-) -> Option<usize> {
-    let mut queue = Vec::from([start]);
-    let mut seen = HashSet::from([start]);
-    let mut reached = 0;
-    while let Some(idx) = queue.pop() {
-        reached += 1;
-        if idx == target {
-            return None;
-        }
-
-        for &adj in &adjacents[idx] {
-            if !forbidden.contains(&(adj.min(idx), adj.max(idx))) && seen.insert(adj) {
-                queue.push(adj);
+            // return partition sizes, if there is no fourth path
+            if let (None, reached) = get_shortest_path(adjacents, &paths, start, target) {
+                return (reached, adjacents.len() - reached);
             }
         }
+
+        panic!("No solution");
     }
-    Some(reached)
 }
 
 pub fn star_1(PuzzleData(adjacents): &PuzzleData) -> usize {
-    // get a target in other part of graph with three disjoint paths
-    let (target, paths) = get_target_with_three_paths(adjacents, 0);
-
-    // find a triple of links, one from every path, so that removal of those
-    // links splits the graph in two
-    let reachable = (0..paths.iter().map(Vec::len).product())
-        .map(|k| {
-            [
-                paths[0][k % paths[0].len()],
-                paths[1][(k / paths[0].len()) % paths[1].len()],
-                paths[2][k / (paths[0].len() * paths[1].len())],
-            ]
-        })
-        .find_map(|forbidden| count_reachable_without_target(adjacents, &forbidden, 0, target))
-        .expect("No solution!");
-
-    // return product of sizes of two disconnected parts of graph
-    reachable * (adjacents.len() - reachable)
+    // get a size of partitions connected by three paths
+    let (p1, p2) = direct_solution::get_partitions_connected_by_three_paths(adjacents, 0);
+    p1 * p2
 }
 // end::star_1[]
 
